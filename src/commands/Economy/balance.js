68975dd81bd51423d018c55705a6813e3ec7f8b2
dadaastra <arafatahmedyeasin@gmail.com>
2026-05-28 04:28:10 +0530

    Update balance.js

--- src/commands/Economy/balance.js
+++ src/commands/Economy/balance.js
@@ -88,7 +88,7 @@ export default {
         // ECONOMY LEADERBOARD POSITION
         // =========================
 
-        let leaderboardPosition = `rank`;
+        let leaderboardPosition = ``;
 
         try {
 
