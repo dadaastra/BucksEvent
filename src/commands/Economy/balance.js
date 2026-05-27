692bdb92a7c976c956a92a2420dfae422d1b6224
dadaastra <arafatahmedyeasin@gmail.com>
2026-05-28 03:54:39 +0530

    Update balance.js

--- src/commands/Economy/balance.js
+++ src/commands/Economy/balance.js
@@ -156,7 +156,7 @@ export default {
         if (targetUser.id === interaction.user.id) {
 
             message =
-                `💵 Currently you have **$${total.toLocaleString()}** micebucks and Your Rank: ${userRank > 0 ? `#${userRank}`;
+                `💵 Currently you have **$${total.toLocaleString()}** micebucks and Your Rank: ${userRank}`;
 
         }
 
