65ff5b4662dbf2034c8e89e68eeb1852c59d6eed
dadaastra <arafatahmedyeasin@gmail.com>
2026-06-04 01:13:01 -1200

    Update errorHandler.js

--- src/utils/errorHandler.js
+++ src/utils/errorHandler.js
@@ -329,7 +329,7 @@ function getErrorTitle(errorType) {
         [ErrorTypes.NETWORK]: "🌐 Network Error",
         [ErrorTypes.DISCORD_API]: "🔌 API Error",
         [ErrorTypes.USER_INPUT]: "💬 Input Error",
-        [ErrorTypes.RATE_LIMIT]: "⏱️ Slow Down!",
+        [ErrorTypes.RATE_LIMIT]: "",
         [ErrorTypes.UNKNOWN]: "❓ Unexpected Error"
     };
     
